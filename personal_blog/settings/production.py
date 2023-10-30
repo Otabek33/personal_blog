5b841e3ae2e8373c056c55b225190b1c534a3a38
Otabek33 <kebatotabek33@gmail.com>
2023-10-30 09:30:32 +0500

    change done

--- personal_blog/settings/production.py
+++ personal_blog/settings/production.py
@@ -5,7 +5,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["otabek-blog.uz"])
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", default=["otabek-blog.uz"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
